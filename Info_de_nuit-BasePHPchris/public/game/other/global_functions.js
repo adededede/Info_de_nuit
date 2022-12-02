c6ab58fc5550694654c02557c6920bd0d228e6cb
ChrisVerschelden <71574021+ChrisVerschelden@users.noreply.github.com>
2022-12-02 04:24:50 +0100

    bouton retour et corrections

--- Info_de_nuit-BasePHPchris/public/game/other/global_functions.js
+++ Info_de_nuit-BasePHPchris/public/game/other/global_functions.js
@@ -1,10 +1,8 @@
 import { g_ctx } from "./global_context.js";
 
 function return_button(){
-    g_ctx.game_mode = false
-    g_ctx.editing_mode = false
-    g_ctx.current_level = 0
-    g_ctx.level_as_been_drawed = false
+    console.log('hey')
+    window.location.href = 'http://lhy.alwaysdata.net/'
 }
 
 export { return_button }
