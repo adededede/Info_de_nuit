9070af19fab9d4ae291064c38bb10170ebd29530
Tricard <adelie@PC-linux>
2022-12-02 02:30:27 +0100

    mise en place d'effets visuels

--- Info_de_nuit-BasePHPchris/app/views/home/home.php
+++ Info_de_nuit-BasePHPchris/app/views/home/home.php
@@ -8,10 +8,24 @@ require '../app/views/layout/menu.php';
     echo '<h1>SISAssociation</h1>';
     echo '<h3>Nuit de l\'info 2022</h3>';
     echo '<p>Voici un site simple et facile d\'utilisation pour en apprendre un maximum sur les maladies sexuellement transmissibles, le consentement et autre sujet pahre d\'une sexualité saine.</p>';
-    echo '<div style="width:500px;">
-        <button type="button" class="msgBtn" onClick="" style="margin-top:100px;height:50px ;width:120px;">Jouer</button>
-        <button type="button" class="msgBtn2" onClick="" style="margin-left: 70px;height:50px ;width:120px;">M\'informer</button>
-    </div>';
-    echo '</center>'; ?>
+
+    $link_address_jouer = '?route=jouer';
+    $link_address_informer = '?route=info';
+
+    echo '<div style="width:500px;">';
+    echo "<a href='$link_address_jouer' class='button' style='
+    margin-top: 100px;
+    height: 50px;
+    width: 120px;
+    margin-left: 70px;' onclick='click_jouer'>Jouer</button>";
+    echo "<a href='$link_address_informer'  class='button' style='
+    margin-top: 100px;
+    height: 50px;
+    width: 120px;
+    margin-left: 70px;' onclick='click_jouer'>M'informer</button>";
+    echo "</div>";
+    echo "</center>";
+
+    ?>
 
 </div>
--- Info_de_nuit-BasePHPchris/app/views/layout/menu.php
+++ Info_de_nuit-BasePHPchris/app/views/layout/menu.php
@@ -1,20 +1,46 @@
 <div class='menu'>
     <?php
+    $home = "";
+    $info = "";
+    $temoin = "";
+    $actu = "";
+    $about = "";
+    $contact = "";
 
+    switch ($_GET['route']) {
+        case 'home':
+            $home = "active";
+            break;
+        case 'info':
+            $info = "active";
+            break;
+        case 'temoin':
+            $temoin = "active";
+            break;
+        case 'contact':
+            $contact = "active";
+            break;
+        case 'news':
+            $news = "active";
+            break;
+        case 'about':
+            $about = "active";
+            break;
+    }
     //ATENTION LEACTIVE NE MARCHE PAS
     echo '<ul>';
     $link_address = '?route=home';
-    echo "<li><a class=\"active\" href='$link_address'>Home</a></li>";
+    echo "<li><a class='$home' href='$link_address'>Home</a></li>";
     $link_address = '?route=info';
-    echo "<li><a href='$link_address'>Informations</a></li>";
+    echo "<li><a class='$info' href='$link_address'>Informations</a></li>";
     $link_address = '?route=temoin';
-    echo "<li><a  href='$link_address'>Témoignages</a></li>";
+    echo "<li><a class='$temoin' href='$link_address'>Témoignages</a></li>";
     $link_address = '?route=news';
-    echo "<li><a  href='$link_address'>Actualités</a></li>";
+    echo "<li><a class='$actu' href='$link_address'>Actualités</a></li>";
     $link_address = '?route=contact';
-    echo "<li><a  href='$link_address'>Contacts</a></li>";
+    echo "<li><a class='$contact' href='$link_address'>Contacts</a></li>";
     $link_address = '?route=about';
-    echo "<li><a  href='$link_address'>A Propos</a></li>";
+    echo "<li><a class='$about' href='$link_address'>A Propos</a></li>";
     echo '</ul>';
 
     ?>
