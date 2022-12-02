3992eb1f20aaf0f0e5410758fe6c3a904c4493fd
Antoine <92716096+AntoineL56@users.noreply.github.com>
2022-12-02 05:20:00 +0100

    bugfix
    bug menu résolue

--- Info_de_nuit-BasePHPchris/app/views/home/temoi.php
+++ Info_de_nuit-BasePHPchris/app/views/home/temoi.php
@@ -4,11 +4,11 @@ require './app/views/layout/menu.php';
 <div class='view'>
 
     <?php
-
+    echo '<h2>Témoignage</h2>';
     //echo '<center>';
     //echo '<h1>Voici les témoignages</h1>';
     //echo '</center>';
-    require 'temoignages.html'
+    require 'temoignages.html';
     ?>
 
 </div>
